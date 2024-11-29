d3fa7b6f7212e83423b7a8ae8b9852ec5bb9b377
RahmetAbdelah <rahmet.abdelah@aastustudent.edu.et>
2024-11-29 21:48:50 +0300

    Create #3. power.cpp

---
+++ 1161-Rahmet-Abdelah/#3. power.cpp
@@ -0,0 +1,17 @@
+#include<iostream>‍
+#include<cmath>‍
+using namespace std;‍
+int main(){‍
+    int base,exponent,value;‍
+    cout<<"please enter the base number: "<<endl;‍
+    cin>>base;‍
+    cout<<"please enter the exponent:"<<endl;‍
+    cin>>exponent;‍
+‍
+‍
+    value=pow(base,exponent);‍
+‍
+    cout<<"the value is: "<<value<<endl;‍
+‍
+‍
+return 0;}
