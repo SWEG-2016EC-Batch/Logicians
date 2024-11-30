9dce650b3393ee6c67561218989bb0a33973a160
RahmetAbdelah <rahmet.abdelah@aastustudent.edu.et>
2024-11-30 08:14:12 +0300

    Create #2 gallon.cpp

---
+++ 1161-Rahmet-Abdelah/#2 gallon.cpp
@@ -0,0 +1,16 @@
+#include <iostream>‍
+using namespace std;‍
+int mai(){‍
+int capacity;‍
+float miles_per_galon;‍
+float miles;‍
+cout << "please enterthe amount of fuel your automobile can have: " << endl;}‍
+cin>>capacity;‍
+cout << "please enter the miles you go per galon:" << endl;‍
+cin>>miles_per_galon;‍
+‍
+miles=capacity*miles per galon;‍
+cout << "The number of miles you can go with out refueuling is " <<miles<< endl;‍
+‍
+‍
+return 0;}
