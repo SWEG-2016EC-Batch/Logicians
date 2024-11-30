8cd3b63b0c162a01b17d650397b1cb6226f425f1
RahmetAbdelah <rahmet.abdelah@aastustudent.edu.et>
2024-11-30 21:15:52 +0300

    Update BMI.cpp

--- 1161-Rahmet-Abdelah/BMI.cpp
+++ 1161-Rahmet-Abdelah/BMI.cpp
@@ -4,19 +4,34 @@ int main(){
    double weight,height,BMI,person;  //declaring variables‍
 ‍
      //accepting input‍
+    a:
     cout<<"for how many persons do you want to calculate the BMI "<<endl;‍
     cin>>person;‍
     if(cin.fail()){
-     cout<<"please enter correct inuput"<<endl;
+      cout<<"please enter correct inuput"<<endl;
      cin.clear();
      cin.ignore();
+     goto a;
     }
 ‍
-    for(int i=1; i<=person; i++){‍
+    for(int i=1; i<=person; i++){
+     ‍b:
     cout << "please enter your weight: " << endl;‍
     cin>>weight;‍
+     if(cin.fail()){
+      cout<<"please enter correct inuput"<<endl;
+     cin.clear();
+     cin.ignore();
+     goto b;}
+     c:
     cout << "please enter your height: " << endl;‍
     cin>>height;‍
+     if(cin.fail()){
+      cout<<"please enter correct inuput"<<endl;
+     cin.clear();
+     cin.ignore();
+     goto c;}
+    
 ‍
     height*=height;//we can use cmath library instead to do the power‍
     BMI=weight/height;‍
@@ -32,7 +47,8 @@ int main(){
      else i‍f(BMI>=24.9){
         cout<<"you are over weight "<<endl;‍
     }‍
-    }‍
+    }
+ ‍cout<<"you have finished your operation,run again if you want to do for more people"
 ‍
 ‍
     return 0;‍
