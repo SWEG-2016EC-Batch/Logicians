efc2b673144a65c138f5e1cd9ac70089035f6317
RahmetAbdelah <rahmet.abdelah@aastustudent.edu.et>
2024-11-30 21:53:39 +0300

    Update #4 salary.cpp

--- 1161-Rahmet-Abdelah/#4 salary.cpp
+++ 1161-Rahmet-Abdelah/#4 salary.cpp
@@ -2,25 +2,51 @@
 using namespace std;‍
 int main(){‍
 string name;‍
-double wwh,br,bs,bp,pb,gs,ns,pd,td;‍
+double weekly_working_hour,bonus_rate,base_salary,bonus_payment;
+double  pension_deduction,gross_salry,net_salary,tat_deduction;‍
 ‍
 ‍
 cout << "please eneter your  name\n" << endl;‍
 cin >> name;‍
+a:
 cout << "please enter your weekly working hour\n " << endl;‍
-cin >> wwh;‍
+cin >> weekly_working_hour;‍
+  if(cin.fail()){
+    cout<<"input valid input"<<endl;
+    cin.clear();
+    cin.ignore();
+    goto a;
+  }
+b:
 cout << "please enter your bonus rate ber hour\n " << endl;‍
-cin >> br;‍
+cin >> bonus_rate;‍
+   if(cin.fail()){
+    cout<<"input valid input"<<endl;
+    cin.clear();
+    cin.ignore();
+    goto b;
+
+  c:
 cout << "please enter your base salary\n " << endl;‍
-cin >> bs;‍
-bp=br*wwh;‍
-gs=bp+bs;‍
-pd=0.05*gs;‍
-td=0.15*gs;‍
-ns=gs-(pd+td);‍
+cin >> base_salary;‍
+
+ if(cin.fail()){
+    cout<<"input valid input"<<endl;
+    cin.clear();
+    cin.ignore();
+    goto c;
+   
+bonus_payment=bonus_rate*weekly_working_hour;‍
+gross_salary=bonus_payment+base_salary;‍
+   
+pension_deduction=0.05*gross_salary;‍
+tax_deduction=0.15*gross_salary;‍
+   
+net_salary=gross_salry-(pension_deduction+tax_deduction);‍
+   
 cout << "dear " <<name<< endl;‍
-cout << "your gross salary adding your bonus payment is:" <<gs<< endl;‍
-cout << "your net salary deducting pention and tax  is:" <<ns<< endl;‍
+cout << "your gross salary adding your bonus payment is:" <<gross_salary<< endl;‍
+cout << "your net salary deducting pention and tax  is:" <<net_salary<< endl;‍
 ‍
 ‍
 return 0;}
