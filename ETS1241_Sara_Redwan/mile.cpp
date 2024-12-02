3cede8a807b8467fe187e814c89aa8aec0def1dd
Sara Redwan Shure <sara.redwan@aastustudent.edu.et>
2024-12-02 13:07:01 +0300

    mile.cpp

--- ETS1241_Sara_Redwan/mile.cpp
+++ ETS1241_Sara_Redwan/mile.cpp
@@ -1,15 +1,33 @@
-#include<iostream>
+
+#include <iostream>
 using namespace std;
-int main(){
-    float tank_capacity, tank_efficiency,total_mile;
-    cout<<"enter the tank capacity in gallon: ";
-    cin>>tank_capacity;
-    cout<<"enter the tank efficiency in mile per gallon: ";
-    cin>>tank_efficiency;
 
-    total_mile=tank_capacity*tank_efficiency;
-
-    cout<<"The automobile can be driven without refueling "<<total_mile<<"mile" ;
+int main()
+{
+    float fuel_capacity,miles_per_gallon,Total_miles_travel;
+    
+    a:
+    cout<<"Enter your fuel tank capacity of the car in gallon"<<endl;
+    cin>>fuel_capacity;
+    if (cin.fail() || fuel_capacity<=0){
+        cin.clear();
+        cin.ignore();
+        cout<<"Invalid input enter again"<<endl;
+        goto a;
+        }
+    b:
+    cout<<"Enter the number of miles per gallon the car be driven"<<endl;
+    cin>>miles_per_gallon;
+    if (cin.fail() || miles_per_gallon<=0){
+        cin.clear();
+        cin.ignore();
+        cout<<"Invalid input enter again"<<endl;
+        goto b;
+       }
+    
+    Total_miles_travel= fuel_capacity*miles_per_gallon;
+    
+    cout<<"The car can be driven without refueling: "<<Total_miles_travel<<"Miles"<<endl;
 
     return 0;
 }
