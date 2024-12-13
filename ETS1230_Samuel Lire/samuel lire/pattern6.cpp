821d84fb8c5e8ea598e968b9f59bccce25993ca9
Samuel Lire <samuellire076@gmail.com>
2024-12-13 14:52:55 +0300

    Create pattern6.cpp

---
+++ ETS1230_Samuel Lire/samuel lire/pattern6.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{int dimension=5;
+for(int i=1;i<=dimension;i++){
+    char character='a';
+    for(int j=1;j<=dimension;j++){
+        cout<<character++<<" ";
+        
+    }
+    cout<<endl;
+}
+    
+
+    return 0;
+}
