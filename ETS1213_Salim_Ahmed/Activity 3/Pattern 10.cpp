97c07ab9a8727bdc0eab09143afe65fc2afaaf22
Salim Ahmed Seid <164552524+salim-ahmed-seid@users.noreply.github.com>
2024-12-13 19:46:25 +0300

    Update Pattern 10.cpp

--- ETS1213_Salim_Ahmed/Activity 3/Pattern 10.cpp
+++ ETS1213_Salim_Ahmed/Activity 3/Pattern 10.cpp
@@ -2,10 +2,14 @@
 using namespace std;
 
 int main()
-{int row_dimension=6, column_dimension=6;
+{int row_dimension, column_dimension;
+cout<<"enter the row dimension\n";
+cin>>row_dimension;
+cout<<"enter the column dimension\n";
+cin>>column_dimension;
 for(int i=1;i<= row_dimension;i++){
     
-   for(int k=6;k>i;k--)
+   for(int k=row_dimension;k>i;k--)
         {cout<<" ";}
     for(int j=1;j<=i;j++){
      cout<<"* "; 
