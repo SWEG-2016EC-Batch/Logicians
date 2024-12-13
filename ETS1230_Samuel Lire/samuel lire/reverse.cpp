46106f6dd5767fb1af3e24bf5573c1a6dca4f7c3
Samuel Lire <samuellire076@gmail.com>
2024-12-13 09:41:26 +0300

    Create reverse.cpp

---
+++ ETS1230_Samuel Lire/samuel lire/reverse.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{ int number,reversed_number=0,remiander,n;
+cout <<"Insert Number You wnat to be reversed"<<endl;
+cin>>n;
+if(n<0){
+    n=-n;
+}
+number=n;
+while(number>0){
+remiander=number%10;
+reversed_number=(reversed_number)*10+(remiander);
+number/=10;}
+cout<<"your reversed number is: "<<reversed_number<<endl;
+    
+
+    return 0;
+} 
