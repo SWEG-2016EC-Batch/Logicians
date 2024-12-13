312db88f440d9bcfe588a165aa6d8f91a2c5ba74
Samuel Lire <samuellire076@gmail.com>
2024-12-13 20:24:27 +0300

    Create pattern7update.cpp

---
+++ ETS1230_Samuel Lire/samuel lire/pattern7update.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{ int row ,column;
+cout<<"enter the row\n";
+cin>>row;
+cout<<"enter the column\n";
+cin>>column;
+   for(int i=1; i<=row;i++){
+       for(int j=1;j<=column;j++){
+           
+           if(i==1 || i==row || j==1 || j==column)cout<<"* ";
+           
+           else cout<<"  ";
+           
+       }cout<<endl;
+   }
+   
+    return 0;
+}  
