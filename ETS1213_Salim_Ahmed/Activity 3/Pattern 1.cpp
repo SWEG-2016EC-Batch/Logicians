7d9348ad1a7675317697ca84142a6cae534878b8
Salim Ahmed Seid <164552524+salim-ahmed-seid@users.noreply.github.com>
2024-12-13 19:48:36 +0300

    Update Pattern 1.cpp

--- ETS1213_Salim_Ahmed/Activity 3/Pattern 1.cpp
+++ ETS1213_Salim_Ahmed/Activity 3/Pattern 1.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 
 int main()
-{int row_dimension=5, column_dimension=5;
+{int row_dimension, column_dimension;
+cout<<"enter the row dimession\n";
+cin>>row_dimension;
+cout<<"enter the column dimession\n";
+cin>>column_dimension;
 for(int i=1;i<= row_dimension;i++){
    int number=1;
     for(int j=1;j<=column_dimension;j++){
