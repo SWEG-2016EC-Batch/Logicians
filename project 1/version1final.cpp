23ed41854e214ebb5c00857e4ca9ff661fb7a3d6
Samuel Lire <samuellire076@gmail.com>
2025-01-06 10:41:26 +0300

    Update and rename Airline Reservation system.cpp to version1final.cpp

--- project 1/version1final.cpp
+++ project 1/version1final.cpp
@@ -1,13 +1,26 @@
+/*   FOP GROUP PROJECT
+
+   Group Member name and Id  
+  Name                      Id
+ 
+1.Paulos Asmelash      ETS1149/16
+2.Rahmet Abdela        ETS1161/16
+3.Salim Ahmed          ETS1213/16
+4.Samuel Lir           ETS1230/16
+5.Sara Redwan          ETS1241/16
+
+*/
 #include <iostream>
 #include <string>
 #include <cctype>
+#include <unistd.h>
 
 using namespace std;
 
 bool firstClassSeats[30] = {false};
 bool economySeats[70] = {false};
-string firstClassPassengerDetails[30];
-string economyPassengerDetails[70];
+string firstClassPassengerDetails[2][3]; //store firstClassPassengerDetails  [][0]: Name, [][1]: Sex, [][2]: Passport Number
+string economyPassengerDetails[2][3]; //store economyPassengerDetails  [][0]: Name, [][1]: Sex, [][2]: Passport Number
 
 int main() {
 	int choice,age;
@@ -17,13 +30,15 @@ int main() {
 	cout<<"********************************************************"<<endl;
 	cout<<"*   Welcome to Logicians Airline Reservation System!   *" << endl;
 	cout<<"********************************************************"<<endl;
-
+	 // Loop to display menu and process user choices,it itrates until the given condtion is false 
 	while (true) {
 		cout << "\nPlease type 1 for First Class" << endl;
 		cout << "Please type 2 for Economy" << endl;
 		cout << "Please type 3 to see available and Occupied seat" << endl;
 		cout << "Please type 4 to cancel a reservation" << endl;
 		cout << "Please type 5 to search for a passenger by name" << endl;
+		cout << "Please type 6 to exit the program" << endl;
+		cout << "********************************************************" << endl;
 		cin >> choice;
 
 		if (choice == 1) {
@@ -66,6 +81,8 @@ int main() {
 					if (sex != 'M' && sex != 'F') { // Validate gender input
 						if (trialCount >0) {
 							cout << "That is not a valid gender! Try To insert letter M for male, letter F for female. You have left "<<trialCount <<" trial"<<endl;
+							cin.clear();
+							cin.ignore(1000, '\n');
 							goto b;
 						}
 						else {
@@ -99,11 +116,13 @@ int main() {
 					trialCount--;
 					bool validPassport = true;
 
-					if (!isalpha(passportNumber[0]) || !isalpha(passportNumber[1])) {  
+					if (!isalpha(passportNumber[0]) || !isalpha(passportNumber[1]))//check the passportNumber contain number and alphabets
+					{  
 						validPassport = false;
 					}
 
-					if (passportNumber.length() != 9) { 
+					if (passportNumber.length() != 9)//check the passportNumber length is 9 
+					{ 
 						validPassport = false;
 					} else {
 						for (char c : passportNumber) {
@@ -124,14 +143,19 @@ int main() {
 						}
 					}
 
-					firstClassPassengerDetails[i] = name;
+				    firstClassPassengerDetails[i][0] = name;//store the name of the Passenger in the first Class
+					firstClassPassengerDetails[i][1] = sex;//store the sex of the Passenger in the first Class
+					firstClassPassengerDetails[i][2] = passportNumber;//store the passportnumber of the Passenger in the first Class
 
 					cout << "Boarding Pass: Seat " << (i + 1) << ", First Class" << endl;
 					cout << "Passenger Details: " << name << ", " << sex << ", " << age << ", " << passportNumber << endl;
+					
+					sleep(3); // Pause the program for 4 seconds to simulate processing time or delay
 					seatAssigned = true; // This is to check whether the inputs are taken as expected
 					break;
 				}
 			}
+            system("clear"); // Clear the console screen to provide a clean display for the next output
 			if (!seatAssigned) {
 				cout << "First Class is fully booked. Would you like to be placed in Economy? (yes/no): ";
 				string response;
@@ -175,6 +199,8 @@ int main() {
 					if (sex != 'M' && sex != 'F') { // Validate gender input
 						if (trialCount >0) {
 							cout << "That is not a valid gender! Try To insert letter M for male, letter F for female. You have left "<<trialCount <<" trial"<<endl;
+							cin.clear();
+							cin.ignore(1000, '\n');
 							goto f;
 						}
 						else {
@@ -233,14 +259,18 @@ int main() {
 						}
 					}
 
-							economyPassengerDetails[i] = name;
-
-							cout << "Boarding Pass: Seat " << (i + 31) << ", Economy" << endl;
+					economyPassengerDetails[i][0] = name;//store the name Passenger in the economy class
+					economyPassengerDetails[i][1] = sex;//store the sex Passenger in the economy class
+					economyPassengerDetails[i][2] = passportNumber;//store the passportNumberof the Passenger in the economy class
+		
+					    	cout << "Boarding Pass: Seat " << (i + 31) << ", Economy" << endl;
 							cout << "Passenger Details: " << name << ", " << sex << ", " << age << ", " << passportNumber << endl;
-							seatAssigned = true;
+							sleep(3); // Pause the program for 4 seconds to simulate processing time or delay
+					        seatAssigned = true;
 							break;
 						}
 					}
+					system("clear"); // Clear the console screen to provide a clean display for the next output
 					if (!seatAssigned) {
 						cout << "Sorry, Economy is also fully booked. Next flight leaves in 3 hours." << endl;
 					}
@@ -287,6 +317,8 @@ int main() {
 					if (sex != 'M' && sex != 'F') { // Validate gender input
 						if (trialCount >0) {
 							cout << "That is not a valid gender! Try To insert letter M for male, letter F for female. You have left "<<trialCount <<" trial"<<endl;
+							cin.clear();
+							cin.ignore(1000, '\n');
 							goto j;
 						}
 						else {
@@ -345,14 +377,18 @@ int main() {
 							return 1;
 						}
 					}
-					economyPassengerDetails[i] = name;
-
+					economyPassengerDetails[i][0] = name;//store the name Passenger in the economy class
+					economyPassengerDetails[i][1] = sex;//store the sex Passenger in the economy class
+					economyPassengerDetails[i][2] = passportNumber;//store the passportNumberof the Passenger in the economy class
+					
 					cout << "Boarding Pass: Seat " << (i + 31) << ", Economy" << endl;
 					cout << "Passenger Details: " << name << ", " << sex << ", " << age << ", " << passportNumber << endl;
+					sleep(3); // Pause the program for 4 seconds to simulate processing time or delay
 					seatAssigned = true;
 					break;
 				}
 			}
+			system("clear"); // Clear the console screen to provide a clean display for the next output
 			if (!seatAssigned) {
 				cout << "Economy is fully booked. Would you like to be placed in First Class? (yes/no): ";
 				string response;
@@ -395,6 +431,8 @@ int main() {
 					if (sex != 'M' && sex != 'F') { // Validate gender input
 						if (trialCount >0) {
 							cout << "That is not a valid gender! Try To insert letter M for male, letter F for female. You have left "<<trialCount <<" trial"<<endl;
+							cin.clear();
+							cin.ignore(1000, '\n');
 							goto n;
 						}
 						else {
@@ -452,14 +490,17 @@ int main() {
 							return 1;
 						}
 					}
-							firstClassPassengerDetails[i] = name;
-                                     //then we print the details
+					firstClassPassengerDetails[i][0] = name;//store the name of the Passenger in the first Class
+					firstClassPassengerDetails[i][1] = sex;//store the sex of the Passenger in the first Class
+					firstClassPassengerDetails[i][2] = passportNumber;//store the passportnumber of the Passenger in the first Class
 						cout << "Boarding Pass: Seat " << (i + 1) << ", First Class" << endl;
 							cout << "Passenger Details: " << name << ", " << sex << ", " << age << ", " << passportNumber << endl;
+							sleep(3); // Pause the program for 4 seconds to simulate processing time or delay
 							seatAssigned = true;
 							break;
 						}
 					}
+					system("clear"); // Clear the console screen to provide a clean display for the next output
 					if (!seatAssigned) {
 						cout << "Sorry, First Class is also fully booked. Next flight leaves in 3 hours." << endl;
 					}
@@ -486,24 +527,22 @@ int main() {
 			getline(cin, enteredPassportNumber);
 
 			if (seatNumber >= 1 && seatNumber <= 30) {
-				if (firstClassSeats[seatNumber - 1] && firstClassPassengerDetails[seatNumber - 1] == enteredPassportNumber) {
+				if (firstClassSeats[seatNumber - 1] && firstClassPassengerDetails[seatNumber - 1][2] == enteredPassportNumber) {
 					firstClassSeats[seatNumber - 1] = false;
-					firstClassPassengerDetails[seatNumber - 1].clear();
+					firstClassPassengerDetails[seatNumber - 1][2].clear();
 					cout << "Reservation for seat " << seatNumber << " in First Class has been canceled." << endl;
 				} else {
 					cout << "Invalid seat number or passport number." << endl;
 				}
 			} else if (seatNumber >= 31 && seatNumber <= 100) {
-				if (economySeats[seatNumber - 31] && economyPassengerDetails[seatNumber - 31] == enteredPassportNumber) {
+				if (economySeats[seatNumber - 31] && economyPassengerDetails[seatNumber - 31][2] == enteredPassportNumber) {
 					economySeats[seatNumber - 31] = false;
-					economyPassengerDetails[seatNumber - 31].clear();
+					economyPassengerDetails[seatNumber - 31][2].clear();
 					cout << "Reservation for seat " << seatNumber << " in Economy has been canceled." << endl;
 				} else {
 					cout << "Invalid seat number or passport number." << endl;
 				}
-			} else {
-				cout << "Invalid seat number." << endl;
-			}
+			} 
 		} else if (choice == 5) {
 			cout << "Enter the name of the passenger to search: ";
 			cin.ignore();
@@ -512,29 +551,32 @@ int main() {
 
 			cout << "Searching in First Class..." << endl;
 			for (int i = 0; i < 30; ++i) {
-				if (firstClassPassengerDetails[i] == name) {
+				if (firstClassPassengerDetails[i][0] == name) {
 					cout << "Passenger found in First Class, Seat " << (i + 1) << endl;
 					passengerFound = true;
 					break;
 				}
 			}
-
 			if (!passengerFound) {
 				cout << "Searching in Economy..." << endl;
 				for (int i = 0; i < 70; ++i) {
-					if (economyPassengerDetails[i] == name) {
+					if (economyPassengerDetails[i][0] == name) {
 						cout << "Passenger found in Economy, Seat " << (i + 31) << endl;
 						passengerFound = true;
 						break;
 					}
 				}
 			}
-
 			if (!passengerFound) {
 				cout << "Passenger not found." << endl;
 			}
-		} else {
-			cout << "Invalid option. Please type 1 for First Class, 2 for Economy, 3 to see available and occupied seats, 4 to cancel a reservation, or 5 to search for a passenger by name." << endl;
+		}
+			else if (choice == 6) {
+			cout << "Exiting the program. Thank you for using our Airline Reservation System!" << endl;
+			break;
+		}
+		 else {
+			cout << "Invalid option. Please type 1 for First Class, 2 for Economy, 3 to see available and occupied seats, 4 to cancel a reservation, 5 to search for a passenger by name or 6 to terminate the program.." << endl;
 		}
 	}
 
