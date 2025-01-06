2f0a822bb8f95ca6ccca3db57640b00310ff83a8
RahmetAbdelah <rahmet.abdelah@aastustudent.edu.et>
2025-01-06 19:13:44 +0300

    Update version1final.cpp

--- project 1/version1final.cpp
+++ project 1/version1final.cpp
@@ -4,7 +4,7 @@
   Name                      Id
  
 1.Paulos Asmelash      ETS1149/16
-2.Rahmet Abdela        ETS1161/16
+2.Rahmet Abdelah       ETS1161/16
 3.Salim Ahmed          ETS1213/16
 4.Samuel Lir           ETS1230/16
 5.Sara Redwan          ETS1241/16
